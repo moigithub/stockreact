c3178a3b8881988cf3671c93955150be13fe2f40
moigithub <mcarlosman@gmail.com>
2017-07-25 11:31:16 -0500

    rebuild

--- server/server.js
+++ server/server.js
@@ -196,7 +196,8 @@ app.route('*').get(function(req,res){
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+//server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 3000,  function(){  
   var addr = server.address();
   console.log("server listening at", addr.address + ":" + addr.port);
 });
